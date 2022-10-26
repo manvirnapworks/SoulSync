19692d4c93a8db6154b4b432c8497296e6dd946d
manvir321 <manvir.napworks@gmail.com>
2022-10-26 11:05:55 +0530

    Initial Commit

---
+++ SoulSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoulSync
+//
+//  Created by Nap Works on 26/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
